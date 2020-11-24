7e0c1d42a1065b940fdc850c3a5bc4b7ec75bbd8
Alejandro Millan <darkslave@live.com>
2020-11-24 15:09:06 -0500

    pwa name update test

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 module.exports = {
+  pwa: {
+    name: "Whatsapp Texter",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
+  },
   pluginOptions: {
     quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: false
-    }
+      importStrategy: "kebab",
+      rtlSupport: false,
+    },
   },
-  transpileDependencies: [
-    'quasar'
-  ]
-}
+  transpileDependencies: ["quasar"],
+};
